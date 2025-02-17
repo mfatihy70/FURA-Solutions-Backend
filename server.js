ef4aa0c1c1c04ffcfeda641aeeaa24713a7ee3f1
mfatihy70 <mfatihyildiz1453@gmail.com>
2025-02-17 21:34:31 +0100

    v0.2.1 fixed prod issues

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
 })
 
 // Start the server
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5050
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`)
 )
